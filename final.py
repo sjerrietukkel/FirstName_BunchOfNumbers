ce3930ad4c276463aea786c4d7695cf4b288bbda
Daniel van der Schuur <danielvdschuur@gmail.com>
2021-10-18 17:35:16 +0200

    fixed pesky bug (returning none from twitter api)

--- final.py
+++ final.py
@@ -5,10 +5,7 @@ from dotenv import dotenv_values
 config = dotenv_values(".env")
 
 with open("data/dutch_names.json", 'r') as file:
-    names = json.load(file)
-
-# initializing string 
-test_str = "Daniel1997"
+    NAMES = json.load(file)
 
 def has_numbers(twitter_handle):
     return any(char.isdigit() for char in twitter_handle)
@@ -16,7 +13,7 @@ def has_numbers(twitter_handle):
 def has_letters(twitter_handle):
     return any(char.isalpha() for char in twitter_handle)
 
-def nameCheck(twitter_handle, amount_of_numbers):  
+def nameCheck(twitter_handle, amount_of_numbers):  # returns bool 
     if has_numbers(twitter_handle) == True:
         if has_letters(twitter_handle) == True:
             temp = re.compile("([a-zA-Z]+)([0-9]+)")    # Using re.compile() + re.match() + re.groups()
@@ -27,13 +24,14 @@ def nameCheck(twitter_handle, amount_of_numbers):
                 bunch_of_numbers = True
             else: 
                 bunch_of_numbers = False
-            for name in names:
-                first_name = False
-                if res[0] == name["name"]:
-                    first_name = True
-                if first_name == True and bunch_of_numbers == True:
-                    exit
-                    return True
+                return False
+            if bunch_of_numbers == True:
+                first_name = res[0]
+                for name in NAMES:
+                    if first_name == name["name"]:
+                        return True
+                else:
+                    return False
             else:
                 return False
         else:
@@ -41,27 +39,27 @@ def nameCheck(twitter_handle, amount_of_numbers):
     else: 
         return False 
 
-# nameCheck(test_str)
-print(nameCheck(test_str, 4))
-
-
 auth = tweepy.OAuthHandler(config["CONSUMER_KEY"], config["CONSUMER_KEY_SECRET"])
 auth.set_access_token(config["ACCESS_TOKEN"], config["ACCESS_TOKEN_SECRET"])
 
 api = tweepy.API(auth)
-# screen_name = "nos"
-query = "#me2"
+query = "Mijn 1ste vliegen zwammen, oh zo blij mee "
 
 data = []
 
 names = []
 
-# for tweets in tweepy.Cursor(api.search_tweets, q=query, count=30, lang="nl", result_type="recent").pages(1):
-#     for tweet in tweets:
-#         tweet = tweet._json
-#         if tweet["user"]["name"] == "WappieNL":
-#             data.append(tweet)
+for tweets in tweepy.Cursor(api.search_tweets, q=query, count=30, lang="nl", result_type="latest").pages(1):
+    for tweet in tweets:
+        tweet = tweet._json
+        twitter_handle = tweet["user"]["screen_name"]
+        name_check = nameCheck(twitter_handle, 2)
+        if name_check == True:
+            print("We caught one!")
+            data.append(tweet)
+        else: 
+            print("No Firstnames Bunchofnumbers found.")
 
-# filename = 'data/tweets_hash.json'
-# with open (filename, 'w') as outfile:
-#     json.dump(data, outfile)
+filename = 'data/tweets_hash.json'
+with open (filename, 'w') as outfile:
+    json.dump(data, outfile)
